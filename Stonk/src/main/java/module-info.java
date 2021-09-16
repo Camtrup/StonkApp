cd53fe7cdb5bcfaed899b534d730e17cecf898bc
casperla <casperla@stud.ntnu.no>
2021-09-16 14:20:39 +0200

    startet på scraper

--- Stonk/src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module stonk {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens stonk to javafx.graphics, javafx.fxml;
-}
--- Stonk/src/main/java/stonk/Stonk.java
+++ Stonk/src/main/java/stonk/Stonk.java
@@ -1,5 +1,21 @@
 package stonk;
 
-public class Stonk {
+import java.io.IOException;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+public class Stonk {
+    public void yeet(String ticker) throws IOException{
+        String link = "https://finance.yahoo.com/quote/" + ticker;
+        Document doc = Jsoup.connect(link+ticker).get();
+        System.out.println(doc);
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        Stonk s = new Stonk();
+        s.yeet("amc");
+    }
 }
+
