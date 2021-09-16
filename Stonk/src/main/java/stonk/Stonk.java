e28b4428b6d107a12f08959e77a445f8ff2787db
casperla <casperla@stud.ntnu.no>
2021-09-16 15:39:23 +0200

    craper funker?

--- Stonk/src/main/java/stonk/Stonk.java
+++ Stonk/src/main/java/stonk/Stonk.java
@@ -2,14 +2,21 @@ package stonk;
 
 import java.io.IOException;
 
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.DomNode;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.javascript.host.html.HTMLElement;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 public class Stonk {
     public void yeet(String ticker) throws IOException{
-        String link = "https://finance.yahoo.com/quote/" + ticker;
-        Document doc = Jsoup.connect(link+ticker).get();
-        System.out.println(doc);
+        String link = "https://www.marketwatch.com/investing/stock/" + ticker;
+        Document doc = Jsoup.connect(link)
+                .cookie("AMCVS_CB68E4BA55144CAA0A4C98A5%40AdobeOrg","1")
+                .get();
+        System.out.println(doc.select("bg-quote.value").first());
     }
 
 
