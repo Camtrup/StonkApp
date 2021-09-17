d78c8a12ba36aa47f9c33cc0a1e248a82a79092c
casperla <casperla@stud.ntnu.no>
2021-09-17 14:29:20 +0200

    satt opp mulig klassestruktur

--- Stonk/src/main/java/stonk/Stonk.java
+++ Stonk/src/main/java/stonk/Stonk.java
@@ -10,23 +10,22 @@ public class Stonk {
     String ticker;
     double price;
 
-
+    //Henter aksjeprisen til "ticker" som bruker søker på
+    //Hvis "ticker" ikke er nøyaktig, får en en NullPointerException siden elementet i HTMLen = NULL
+    //en blir en viderført til en liste med lignende aksjer, der velger en bare den første og satser på at det er riktig
     public double getTickerPrice(String ticker) throws IOException{
-        //Henter aksjeprisen til "ticker" som bruker søker på
+        this.ticker = ticker;
         String link = "https://www.marketwatch.com/investing/stock/" + ticker;
         Document doc = Jsoup.connect(link)
                 .cookie("AMCVS_CB68E4BA55144CAA0A4C98A5%40AdobeOrg","1")
                 .get();
         try {
-            return Double.parseDouble(doc.select("bg-quote.value").first().text());
+            return this.price = Double.parseDouble(doc.select("bg-quote.value").first().text());
         }
         catch (NullPointerException e){
-            //Hvis "ticker" ikke er nøyaktig, får en en error siden elementet i HTMLen = NULL
-            //en blir en viderført til en liste med lignende aksjer, der velger en bare den første og satser på at det er riktig
             ticker = doc.select(".results table tbody tr td a").first().text();
             return getTickerPrice(ticker);
         }
-
     }
 
 
---
+++ Stonk/src/main/java/stonk/User.java
@@ -0,0 +1,7 @@
+package stonk;
+
+public class User {
+    String username;
+    String password;
+
+}
