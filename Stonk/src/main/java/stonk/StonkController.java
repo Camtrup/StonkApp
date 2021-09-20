f4185f84c0d2b254c211d95a7ed834cd85658592
Tagegit <itouhc@gmail.com>
2021-09-20 19:22:24 +0200

    Fikset så appen kjører

--- Stonk/src/main/java/stonk/StonkController.java
+++ Stonk/src/main/java/stonk/StonkController.java
@@ -1,9 +1,50 @@
 package stonk;
 
-import javafx.fxml.FXML;
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage; 
+import javafx.scene.Node;
+
 
 
 public class StonkController {
+
+    private Stage stage; 
+    private Scene scene; 
+    private Parent parent;
+
+    public void fromLoginToRegister(ActionEvent event) throws IOException{
+        Parent fxmlLoader = FXMLLoader.load(getClass().getResource("newUser.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(fxmlLoader);
+        stage.setScene(scene);
+        stage.show();
+
+
+    }
+    public void fromLoginToMain(ActionEvent event) throws IOException{
+        Parent fxmlLoader = FXMLLoader.load(getClass().getResource("mainPage.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(fxmlLoader);
+        stage.setScene(scene);
+        stage.show();
+
+
+    }
+
+    public void fromRegisterToMain(ActionEvent event) throws IOException{
+        Parent fxmlLoader = FXMLLoader.load(getClass().getResource("mainPage.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(fxmlLoader);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
 
 
 }
