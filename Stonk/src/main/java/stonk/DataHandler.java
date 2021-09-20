81559dd814a5516b1a6c52e9623b5a919584fb4b
casperla <casperla@stud.ntnu.no>
2021-09-20 23:31:11 +0200

    muligens ferdig med datahndler

--- Stonk/src/main/java/stonk/DataHandler.java
+++ Stonk/src/main/java/stonk/DataHandler.java
@@ -147,7 +147,6 @@ public class DataHandler {
                 containsStock = true;
                 stock = x;
             }
-            index++;
         }
         if(containsStock){
         
