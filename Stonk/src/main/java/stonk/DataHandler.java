87a274ccfa510a70fda69015375e749e4274ceac
casperla <casperla@stud.ntnu.no>
2021-09-20 14:57:28 +0200

    findUser i fil

--- Stonk/src/main/java/stonk/DataHandler.java
+++ Stonk/src/main/java/stonk/DataHandler.java
@@ -4,7 +4,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.StringWriter;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -13,9 +12,14 @@ import org.json.simple.parser.ParseException;
 
 public class DataHandler {
     
-    public void newUser(String username, String password, String firstname, String lastname) throws ParseException, FileNotFoundException, IOException {
+    public void newUser(String username, String password, String firstname, String lastname, int age, int cash) {
         JSONParser parser = new JSONParser();
-        Object fileReaderObj = parser.parse(new FileReader("Stonk/src/main/resources/app/database.json"));
+        Object fileReaderObj = null;
+        try {
+            fileReaderObj = parser.parse(new FileReader("Stonk/src/main/resources/app/database.json"));
+        } catch (IOException | ParseException e1) {
+            e1.printStackTrace();
+        }
         JSONObject jsonObject = (JSONObject) fileReaderObj;
         JSONArray userArray = getAllUsers();
         
@@ -25,15 +29,11 @@ public class DataHandler {
         user.put("password",password);
         user.put("firstname",firstname);
         user.put("lastname",lastname);
-        user.put("cash","");
+        user.put("age", age);
+        user.put("cash", cash);
         user.put("lists","");
 
-        JSONObject userSuper = new JSONObject();
-        String userCount = "user" + (getUserCount()+1);
-
-        userSuper.put(userCount, user);
-
-        userArray.add(userSuper);
+        userArray.add(user);
         jsonObject.put("users", userArray);
        
         try (FileWriter file = new FileWriter("Stonk/src/main/resources/app/database.json", false)){
@@ -46,34 +46,39 @@ public class DataHandler {
         }
     }
 
-    public JSONArray getAllUsers() throws FileNotFoundException, IOException, ParseException{
-        JSONObject jsonObject = (JSONObject)new JSONParser().parse(new FileReader("Stonk/src/main/resources/app/database.json"));
+    public JSONArray getAllUsers(){
+        JSONObject jsonObject = null;
+        try {
+            jsonObject = (JSONObject)new JSONParser().parse(new FileReader("Stonk/src/main/resources/app/database.json"));
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
         JSONArray userArray = (JSONArray) jsonObject.get("users");
         return userArray;
     }
 
-    public int getUserCount() throws FileNotFoundException, IOException, ParseException{
-        JSONObject jsonObject = (JSONObject)new JSONParser().parse(new FileReader("Stonk/src/main/resources/app/database.json"));
-        JSONArray userArray = (JSONArray) jsonObject.get("users");
-        return userArray.size();
+    public int getUserCount() {
+        return getAllUsers().size();
     }
 
-    public int findUser(String username) throws FileNotFoundException, IOException, ParseException{
+    public int findUser(String username) {
         JSONArray userArray = getAllUsers();
-        for(int i = 0; i < getUserCount(); i++){
-            JSONObject x = (JSONObject) userArray.get(i).get("")
+        int count = 0;
+        for(Object i : userArray){
+            JSONObject user = (JSONObject) i;
+            if(user.get("username")==username){
+                return count;
+            }
+            count++;
         }
-
-
-        return (Integer) null;
+        return getUserCount() + 1;
     }
 
     public User getUser(int index){
-        return new User();
+        return new User("","","","", index, index);
     }
-
-    public static void main(String[] args) throws IOException, ParseException {
+    public static void main(String[] args){
         DataHandler d = new DataHandler();
-        d.newUser("casper", "12345", "Casper", "SASAS");
+        d.findUser("username");
     }
 }
