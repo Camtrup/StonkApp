9405e2ea175ad473edc7233cad3c344f51192890
Tagegit <itouhc@gmail.com>
2021-09-21 16:53:53 +0200

    Added icon and title to app

--- Stonk/src/main/java/stonk/StonkApp.java
+++ Stonk/src/main/java/stonk/StonkApp.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,6 +18,8 @@ public class StonkApp extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("login.fxml"));
         Parent parent = fxmlLoader.load();
+        stage.getIcons().add(new Image("stonkpicture.png"));
+        stage.setTitle("Stonk");
         stage.setScene(new Scene(parent));
         stage.show();
     }
