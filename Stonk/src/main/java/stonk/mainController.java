bd32c2ddb47c3d15d35910c9c8fb4d757106ca32
Tagegit <itouhc@gmail.com>
2021-09-23 10:43:41 +0200

    Minor bugfix in controller and main-fxml

--- Stonk/src/main/java/stonk/mainController.java
+++ Stonk/src/main/java/stonk/mainController.java
@@ -1,6 +1,31 @@
 package stonk;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class mainController {
+
+    private User user;
+    
+    @FXML
+    private TextField cashMoneyFlow; 
+
+    @FXML
+    public void displayCash(){
+        cashMoneyFlow.setText(Float.toString(user.getCash()));
+    }
+
+    public void setUser(User user){
+        this.user = user; 
+    }
+    
+    public static void main(String[] args) {
+        mainController controller = new mainController();
+        controller.displayCash();
+    }
+
+
+
 
     
 
