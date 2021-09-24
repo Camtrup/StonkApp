b9dd356ac6af47af53f9d8ebb8da9bc07712919d
casperla <casperla@stud.ntnu.no>
2021-09-24 11:10:20 +0200

    UserTest runs and works

--- Stonk/src/test/java/stonk/UserTest.java
+++ Stonk/src/test/java/stonk/UserTest.java
@@ -14,35 +14,21 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class StonkAppTest {
+public class UserTest {
 
-	private Stonk stonk;
-
+	private User user;
+    
 	@BeforeEach
-	public void setup() {
-		this.stonk = new Stonk();
+    public void setup(){
+        user = new User("Tage", "Berg", "tagemb", "123", 10000, 23, null, false );
+    }
 
-	}
+    @Test
+    public void testAge(){
+        assertThrows(IllegalArgumentException.class,() -> {
+            user.setAge(17);
+        });
+    }
 
-	@Test
-	@DisplayName("Constructor Test")
-	public void teststonk() {
-
-	}
-
-	@Test
-	@DisplayName("age over 18")
-	public void testAge() {
-	
-
-	}
-
-
-
-	@Test
-	@DisplayName("Changes done?")
-	public void testCheckIdentical() {
-	
-	}
 
 }
