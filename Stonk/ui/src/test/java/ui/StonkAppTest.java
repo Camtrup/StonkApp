a1f84b212c037b1fe63aaa91fe6ed0e23628f486
casperla <casperla@stud.ntnu.no>
2021-10-14 11:20:56 +0200

    Maven test now works

--- Stonk/ui/src/test/java/ui/StonkAppTest.java
+++ Stonk/ui/src/test/java/ui/StonkAppTest.java
@@ -1,40 +1,31 @@
 package ui;
-
-import java.io.IOException;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.testfx.framework.junit5.ApplicationTest;
-
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+import org.testfx.framework.junit5.ApplicationTest;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class StonkAppTest extends ApplicationTest{
-    
-    private StonkApp controller;
-    private Parent root;
+public class StonkAppTest extends ApplicationTest {
+    private loginController controller;
 
     @Override
-    public void start(final Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("main/resources/ui/fxml/login.fxml"));
-        System.out.println(fxmlLoader.getClass().toString());
-        root = fxmlLoader.load();
-        controller = fxmlLoader.getController();
+    public void start(final Stage stage) throws Exception{
+        final FXMLLoader loader = new FXMLLoader(getClass().getResource("fxml/login.fxml"));
+        final Parent root = loader.load();
+        this.controller = loader.getController();
         stage.setScene(new Scene(root));
         stage.show();
-    } 
+    }
 
     @Test
     public void testLogin(){
-        // String username = "casper";
-        // String password = "12345";
-        // clickOn("#username").write(username);
-        // clickOn("#password").write(password);
-        // clickOn("#login");
-        //root.lookup("#login").toString();
-        System.out.println("eyeyeyeyes");
+        System.out.println("asdasd");
+        clickOn("#username").write("casper");
+        clickOn("#password").write("12345");
+        clickOn("#login");
     }
-
+    
 }
