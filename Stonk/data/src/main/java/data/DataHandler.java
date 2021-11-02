d5405d0b9528777f39f396c5bfd88015193b5f85
Tagegit <itouhc@gmail.com>
2021-11-02 13:38:10 +0100

    fixed checkstyle for data

--- Stonk/data/src/main/java/data/DataHandler.java
+++ Stonk/data/src/main/java/data/DataHandler.java
@@ -5,226 +5,282 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.net.URI;
-import java.net.URL;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.net.URI;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-
+/**
+ * Class datahandler.
+ */
 public class DataHandler {
 
-    //The filepath now dynamiclly finds the working directory of the user, and the adds the path to the database
-    private String file = System.getProperty("user.dir") + "/Stonk/data/src/main/resources/database.json";
-    //When running with "javafx:run", the working directory will be "ui".
-    //This method removes the path into "ui", so the path finds the file in "data"
-    private void adaptFilePath(){
-        String s = file.replace("/Stonk/ui", "");
-        file = s;
+  // The filepath now dynamiclly finds the working directory of the user, and the
+  // adds the path to the database
+  private String file = System.getProperty("user.dir") + "/Stonk/data/src/"
+      + "main/resources/database.json";
+
+  // When running with "javafx:run", the working directory will be "ui".
+  // This method removes the path into "ui", so the path finds the file in "data"
+  private void adaptFilePath() {
+    String s = file.replace("/Stonk/ui", "");
+    file = s;
+  }
+
+  /**
+   * Creates a new user in the database.
+   *
+   * @param username as string
+   * @param password as string
+   * @param firstname as string
+   * @param lastname as string 
+   * @param age int
+   * @param cash float
+   * @param portfolio JSONArray
+   */
+  public void newUser(String username,
+      String password, String firstname, String lastname, int age, float cash,
+      JSONArray portfolio) {
+    JSONObject user = new JSONObject();
+    user.put("username", username);
+    user.put("password", password);
+    user.put("firstname", firstname);
+    user.put("lastname", lastname);
+    user.put("age", age);
+    user.put("cash", cash);
+    user.put("portfolio", portfolio);
+    JSONArray userArray = getAllUsers();
+    userArray.add(user);
+    writeToFile(userArray);
+  }
+
+  /**
+   * Get all users.
+   *
+   * @return array of all users.
+   */
+  public JSONArray getAllUsers() {
+    adaptFilePath();
+    JSONParser parser = new JSONParser();
+    JSONArray userArray = new JSONArray();
+    try (FileReader reader = new FileReader(file, StandardCharsets.UTF_8)) {
+      JSONObject obj = (JSONObject) parser.parse(reader);
+      userArray = (JSONArray) obj.get("users");
+    }  catch (IOException | ParseException e) {
+      System.out.println(e);
     }
-    
-    //Creates new user in the database
-    public void newUser(String username, String password, String firstname, String lastname, int age, float cash, JSONArray portfolio) {
-        JSONArray userArray = getAllUsers();
-        JSONObject user = new JSONObject();
-        user.put("username",username);
-        user.put("password",password);
-        user.put("firstname",firstname);
-        user.put("lastname",lastname);
-        user.put("age", age);
-        user.put("cash", cash);
-        user.put("portfolio",portfolio);
+    return userArray;
+  }
 
-        userArray.add(user);
-        writeToFile(userArray);
+  /**
+   * Returns a given user in database by checking for unique username.
+   *
+   * @param username string
+   * @return user as JSONObject.
+   */
+  public JSONObject findUser(String username) {
+    JSONArray userArray = getAllUsers();
+    for (Object i : userArray) {
+      JSONObject user = (JSONObject) i;
+      if (username.equals(user.get("username").toString())) {
+        return user;
+      }
     }
+    return null;
+  }
 
-    //Array of all users
-    public JSONArray getAllUsers(){
-        adaptFilePath();
-        JSONParser parser = new JSONParser();
-        JSONArray userArray = new JSONArray();
-        try(FileReader reader = new FileReader(file, StandardCharsets.UTF_8)){
-            JSONObject obj = (JSONObject) parser.parse(reader);
-            userArray = (JSONArray) obj.get("users");
-        }
-        
-        catch(IOException | ParseException e){
-            System.out.println(e);
-        }
-        return userArray;
-    }
+  /**
+   * gets a given portfolio.
+   *
+   * @param user given user.
+   * @return a users portfolio.
+   */
+  public JSONArray getPortfolio(JSONObject user) {
+    JSONArray portfolio = (JSONArray) user.get("portfolio");
+    return portfolio;
+  }
 
-    //Returns a given user in the database by checking for the unique username
-    //If the username is not found, then return null
-    public JSONObject findUser(String username) {
-        JSONArray userArray = getAllUsers();
-        for(Object i : userArray){
-            JSONObject user = (JSONObject) i;
-            if(username.equals(user.get("username").toString())){
-                return user;
-            }
-        }
-        return null;
-    }
+  /**
+   * adds stock to portfolio.
+   *
+   * @param username string.
+   * @param ticker string for finding stock.
+   * @param price from scraper.
+   * @param count how many.
+   */
+  public void addToPortfoilio(String username, String ticker, float price, int count) {
+    JSONArray userArray = getAllUsers();
+    JSONObject user = (JSONObject) userArray.get(userArray.indexOf(findUser(username)));
+    JSONArray portfolio = getPortfolio(user);
+    JSONObject stock = new JSONObject();
 
-    //Returs a user portfolio
-    public JSONArray getPortfolio(JSONObject user){
-        JSONArray portfolio = (JSONArray) user.get("portfolio");
-        return portfolio;
-    }
+    boolean containsStock = false;
 
-
-    //Checks if portfolio containsStock(user has bought this stock earlier)
-    //If it has, the new price is the average, with a count += newCount
-    //If not, it adds the new stock in the portfolio
-    public void addToPortfoilio(String username, String ticker, float price, int count){
-        JSONArray userArray = getAllUsers();
-        JSONObject user = (JSONObject) userArray.get(userArray.indexOf(findUser(username)));
-        JSONArray portfolio = getPortfolio(user);
-        JSONObject stock = new JSONObject();
-        
-        boolean containsStock = false;
-        
-        for(Object i : portfolio){
-            JSONObject checkStock = (JSONObject) i;
-            if(checkStock.get("ticker").equals(ticker)){
-                stock = checkStock;
-                containsStock = true;
-                break;
-            }
-        }
-        
-        if(containsStock){
-            float oldPrice = (float) Double.parseDouble(stock.get("price").toString());
-            int oldCount = Integer.parseInt(stock.get("count").toString());
-            
-            int newCount = oldCount + count;
-            float newPrice = (oldPrice * oldCount + price * count)/newCount;
-
-            stock.put("price", newPrice);
-            stock.put("count", newCount);
-        }
-        else {
-            stock.put("ticker", ticker);
-            stock.put("price", price);
-            stock.put("count", count);
-            portfolio.add(stock);
-        }
-
-        float currentCash = Float.parseFloat(user.get("cash").toString());
-
-        user.put("cash", currentCash - price * count);
-        user.put("portfolio", portfolio);
-        writeToFile(userArray);
-    }
-    public void addOrRemoveCash(String username, float money){
-        JSONArray userArray = getAllUsers();
-        JSONObject user = (JSONObject) userArray.get(userArray.indexOf(findUser(username)));
-        float currentCash = Float.parseFloat(user.get("cash").toString());
-        user.put("cash", currentCash + money);
-        writeToFile(userArray);
+    for (Object i : portfolio) {
+      JSONObject checkStock = (JSONObject) i;
+      if (checkStock.get("ticker").equals(ticker)) {
+        stock = checkStock;
+        containsStock = true;
+        break;
+      }
     }
 
+    if (containsStock) {
+      float oldPrice = (float) Double.parseDouble(stock.get("price").toString());
+      int oldCount = Integer.parseInt(stock.get("count").toString());
 
-    //Checks if the amount of stocks is valid, and then subtracts from the user
-    //If one sells the full amount of stocks, the stock is removed from the portfolio
-    //Adds the cash back in the account
-    public void removeFromPortfolio(String username, String ticker,float price, int count){
-        JSONArray userArray = getAllUsers();
-        JSONObject user = (JSONObject) userArray.get(userArray.indexOf(findUser(username)));
-        JSONArray portfolio = (JSONArray) user.get("portfolio");
-        JSONObject stock = new JSONObject();
+      int newCount = oldCount + count;
+      float newPrice = (oldPrice * oldCount + price * count) / newCount;
 
-        boolean containsStock = false;
-
-        for(Object i : portfolio){
-            JSONObject x = (JSONObject) i;
-            if(x.get("ticker").equals(ticker)){
-                containsStock = true;
-                stock = x;
-            }
-        }
-        if(containsStock){
-            int newCount = Integer.parseInt(stock.get("count").toString()) - count;
-            if(newCount >= 0){
-                if(newCount == 0){
-                    portfolio.remove(stock);
-                }
-                else {
-                    stock.put("count", newCount);
-                }
-            }
-            else {
-                throw new IllegalArgumentException("Not enough stocks to sell");
-            }
-            float currentCash = Float.parseFloat(user.get("cash").toString());
-            user.put("cash", currentCash + price * count);
-            user.put("portfolio", portfolio);
-            writeToFile(userArray);
-        }
-        else {
-            throw new IllegalArgumentException("Stock not in portfolio");
-        }
+      stock.put("price", newPrice);
+      stock.put("count", newCount);
+    } else {
+      stock.put("ticker", ticker);
+      stock.put("price", price);
+      stock.put("count", count);
+      portfolio.add(stock);
     }
 
-    //Returns the users cash
-    public float getCash(int userIndex){
-        JSONArray userArray = getAllUsers();
-        JSONObject user = (JSONObject) userArray.get(userIndex);
-        return Float.parseFloat(user.get("cash").toString());
+    float currentCash = Float.parseFloat(user.get("cash").toString());
+
+    user.put("cash", currentCash - price * count);
+    user.put("portfolio", portfolio);
+    writeToFile(userArray);
+  }
+
+  /**
+   * Adds or removes cash.
+   *
+   * @param username string.
+   * @param money float.
+   */
+  public void addOrRemoveCash(String username, float money) {
+    JSONArray userArray = getAllUsers();
+    JSONObject user = (JSONObject) userArray.get(userArray.indexOf(findUser(username)));
+    float currentCash = Float.parseFloat(user.get("cash").toString());
+    user.put("cash", currentCash + money);
+    writeToFile(userArray);
+  }
+
+  // Checks if the amount of stocks is valid, and then subtracts from the user
+  // If one sells the full amount of stocks, the stock is removed from the
+  // portfolio
+  // Adds the cash back in the account
+  /**
+   * Checks if the amount of stocks is valid, and then subtracts from user.
+   *
+   * @param username string.
+   * @param ticker string.
+   * @param price float.
+   * @param count int.
+   */
+  public void removeFromPortfolio(String username, String ticker, float price, int count) {
+    JSONArray userArray = getAllUsers();
+    JSONObject user = (JSONObject) userArray.get(userArray.indexOf(findUser(username)));
+    JSONArray portfolio = (JSONArray) user.get("portfolio");
+    JSONObject stock = new JSONObject();
+
+    boolean containsStock = false;
+
+    for (Object i : portfolio) {
+      JSONObject x = (JSONObject) i;
+      if (x.get("ticker").equals(ticker)) {
+        containsStock = true;
+        stock = x;
+      }
     }
-
-/*     public void addCash(User user, float cash){
-        JSONObject user = (JSONObject) userArray.get(userIndex);
-        Float a = (cash + user.getCash());
-    } */
-
-
-    //Throws Excpetion if username doesnt exists
-    //Returns null if password is incorrect
-    //Returns a new instance of a user if the login is valid
-    public JSONObject isLoginValid(String username, String password){
-        JSONObject user = findUser(username);
-        if(user != null){
-            if(user.get("password").toString().equals(password)){
-                return user;
-            }
-            else {
-                throw new IllegalArgumentException("Password is incorrect");
-            }
+    if (containsStock) {
+      int newCount = Integer.parseInt(stock.get("count").toString()) - count;
+      if (newCount >= 0) {
+        if (newCount == 0) {
+          portfolio.remove(stock);
+        } else {
+          stock.put("count", newCount);
         }
-        else {
-            throw new IllegalArgumentException("Username not in databse");
-        }
+      } else {
+        throw new IllegalArgumentException("Not enough stocks to sell");
+      }
+      float currentCash = Float.parseFloat(user.get("cash").toString());
+      user.put("cash", currentCash + price * count);
+      user.put("portfolio", portfolio);
+      writeToFile(userArray);
+    } else {
+      throw new IllegalArgumentException("Stock not in portfolio");
     }
+  }
 
-    public void deleteUser(String username){
-        JSONArray arr = getAllUsers();
-        JSONObject user = findUser(username);
-        arr.remove(user);
-        writeToFile(arr);
-    }
+  /**
+   * getCash method.
+   *
+   * @param userIndex for finding user.
+   * @return the users cash.
+   */
+  public float getCash(int userIndex) {
+    JSONArray userArray = getAllUsers();
+    JSONObject user = (JSONObject) userArray.get(userIndex);
+    return Float.parseFloat(user.get("cash").toString());
+  }
 
-    //Writes the array to the file
-    public void writeToFile(JSONArray arr){
-        adaptFilePath();
-        JSONObject obj = new JSONObject();
-        obj.put("users", arr);
-        try(FileOutputStream fileStream = new FileOutputStream(file, false)){
-            Writer writer = new OutputStreamWriter(fileStream, StandardCharsets.UTF_8);
-            writer.write(obj.toJSONString()); 
-            writer.close();
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
+  /*
+   * public void addCash(User user, float cash){ JSONObject user = (JSONObject)
+   * userArray.get(userIndex); Float a = (cash + user.getCash()); }
+   */
+
+  /**
+   * Throws exception if username does not exist, and null if incorrect.
+   *
+   * @param username string
+   * @param password string
+   * @return new instance of user if login is valid.
+   */
+  public JSONObject isLoginValid(String username, String password) {
+    JSONObject user = findUser(username);
+    if (user != null) {
+      if (user.get("password").toString().equals(password)) {
+        return user;
+      } else {
+        throw new IllegalArgumentException("Password is incorrect");
+      }
+    } else {
+      throw new IllegalArgumentException("Username not in databse");
     }
+  }
+
+  /**
+   * Deletes a given user.
+   *
+   * @param username for finding user.
+   */
+  public void deleteUser(String username) {
+    JSONArray arr = getAllUsers();
+    JSONObject user = findUser(username);
+    arr.remove(user);
+    writeToFile(arr);
+  }
+
+  // Writes the array to the file
+  /**
+   * Writes the array to file.
+   *
+   * @param arr given array.
+   */
+  public void writeToFile(JSONArray arr) {
+    adaptFilePath();
+    JSONObject obj = new JSONObject();
+    obj.put("users", arr);
+    try (FileOutputStream fileStream = new FileOutputStream(file, false)) {
+      Writer writer = new OutputStreamWriter(fileStream, StandardCharsets.UTF_8);
+      writer.write(obj.toJSONString());
+      writer.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 
 }
--- Stonk/data/src/main/java/data/data.java
+++ Stonk/data/src/main/java/data/data.java
@@ -4,28 +4,36 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+/**
+ * Class data.
+ */
 public class data {
-    private String filePath = getClass().getClassLoader().getResource("database.json").toString();
-    private File file = new File(getClass().getClassLoader().getResource("database.json").getFile());
+  private String filePath = getClass().getClassLoader().getResource("database.json").toString();
+  private File file = new File(getClass().getClassLoader().getResource("database.json").getFile());
 
-    private String path = System.getProperty("user.dir") + "/Stonk/data/src/main/resources/database.json";
+  private String path = System.getProperty("user.dir")
+      + "/Stonk/data/src/main/resources/database.json";
 
-    JSONParser parser = new JSONParser();
+  JSONParser parser = new JSONParser();
 
-    public static void main(String[] args) throws IOException, ParseException {
-        data d = new data();
-        System.out.println(d.file);
-        
+  /**
+   * Main method. 
+   *
+   * @param args .
+   * @throws IOException .
+   * @throws ParseException .
+   */
+  public static void main(String[] args) throws IOException, ParseException {
+    data d = new data();
+    System.out.println(d.file);
 
-        FileReader f = new FileReader(d.path);
-        JSONObject s = (JSONObject) d.parser.parse(f);
-        System.out.println(s.toJSONString());
+    FileReader f = new FileReader(d.path);
+    JSONObject s = (JSONObject) d.parser.parse(f);
+    System.out.println(s.toJSONString());
 
-
-    }
+  }
 }
